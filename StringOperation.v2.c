0b5eba7dc259e2498839def6d7adf5ecc1a8bae8
bitpeng <bitpeng@yeah.net>
2015-09-02 00:05:34 +0800

    字符串操作与算法

---
+++ StringOperation.v2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+
+//两个字符串str1，str2，找出str2在str1中的位置。
+int strstr(char *s1, char *s2)
+{}
+
+//  最长重复子串
+int LongestCommonSubstr(char *s1, char *s2)
+{
+
+}
+
+/**
+* 二分查找，查找target，在区间[start，end]之间
+* 有重复元素，返回最后一个下标
+* 其他情况返回-1
+*/
+int bisearch(vector<int> arr, int len, int target, int start, int end)
+{
+    
+}
+ 
+ 
+/**
+* 输出字符串中的所有重复子串：
+* 例如：abcab
+* 输出: a, b, ab
+*
+*/ 
+void getAllSub(string str)
+{
+ 
+}
